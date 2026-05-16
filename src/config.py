e48a4c5d2966f1c57d4467891db9a1e6e94e51ca
BielNietoJuanes <bielnieto.develop@gmail.com>
2026-05-16 18:59:43 +0200

    feat: add dataset and configure paths

--- src/config.py
+++ src/config.py
@@ -2,6 +2,5 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
 
-# Change these paths to point to your data files
-RAW_PATH = ROOT / "data" / "raw" / "raw_dataset.csv"
-OUT_PATH = ROOT / "data" / "processed" / "clean_dataset.csv"
+RAW_PATH = "data/raw/hotel_bookings.csv"
+OUT_PATH = "data/processed/clean_dataset.csv"
